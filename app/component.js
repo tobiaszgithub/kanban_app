f5c243ad4c595b919e19b3823495d2e2312e12fb
tobiaszgithub <tobiasz.github@gmail.com>
2016-02-14 20:50:03 +0100

    Webpack and react integration

--- app/component.js
+++
@@ -1,5 +0,0 @@
-module.exports = function(){
-	var element = document.createElement('h2');
-	element.innerHTML = 'Hello world3';
-	return element;
-};
--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,67 @@
 const path = require('path');
 const merge = require('webpack-merge');
 const webpack = require('webpack');
+const NpmInstallPlugin = require('npm-install-webpack-plugin');
 
 const TARGET = process.env.npm_lifecycle_event;
 const PATHS = {
-	app: path.join(__dirname, 'app'),
-	build: path.join(__dirname, 'build')
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'build')
 };
+
+process.env.BABEL_ENV = TARGET;
+
 const common = {
-	entry: {
-		app: PATHS.app
-	},
-	output: {
-		path: PATHS.build,
-		filename: 'bundle.js'
-	}
+  entry: {
+    app: PATHS.app
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  output: {
+    path: PATHS.build,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loaders: ['style', 'css'],
+        include: PATHS.app
+      },
+      {
+        test: /\.jsx?$/,
+        loaders: ['babel?cacheDirectory'],
+        include: PATHS.app
+      }
+    ]
+  }
 };
 
-// Default configuration
 if(TARGET === 'start' || !TARGET) {
-
   module.exports = merge(common, {
+    devtool: 'eval-source-map',
     devServer: {
       contentBase: PATHS.build,
 
-      // Enable history API fallback so HTML5 History API based
-      // routing works. This is a good default that will come
-      // in handy in more complicated setups.
       historyApiFallback: true,
       hot: true,
       inline: true,
       progress: true,
 
-      // Display only errors to reduce the amount of output.
+      // display only errors to reduce the amount of output
       stats: 'errors-only',
 
-      // Parse host and port from env so this is easy to customize.
-      //
-      // If you use Vagrant or Cloud9, set
-      // host: process.env.HOST || '0.0.0.0';
-      //
-      // 0.0.0.0 is available to all network devices unlike default
-      // localhost
+      // parse host and port from env so this is easy
+      // to customize
       host: process.env.HOST,
       port: process.env.PORT
     },
     plugins: [
-      new webpack.HotModuleReplacementPlugin()
+      new webpack.HotModuleReplacementPlugin(),
+      new NpmInstallPlugin({
+        save: true // --save
+      })
     ]
   });
 }
@@ -54,4 +69,3 @@ if(TARGET === 'start' || !TARGET) {
 if(TARGET === 'build') {
   module.exports = merge(common, {});
 }
-
