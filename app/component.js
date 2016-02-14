24b348ae9197f2c57812c04fdc16aaf0023cbd01
tobiaszgithub <tobiasz.github@gmail.com>
2016-02-14 17:46:16 +0100

    Configuring Hot Module Replacement (HMR)

--- app/component.js
+++ app/component.js
@@ -1,5 +1,5 @@
 module.exports = function(){
-	var element = document.createElement('h1');
-	element.innerHTML = 'Hello world';
+	var element = document.createElement('h2');
+	element.innerHTML = 'Hello world3';
 	return element;
 };
