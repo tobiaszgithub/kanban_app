d5bfb7a9f3a4f7f0a4bd32b2dfe3ac90c745125d
tobiaszgithub <tobiasz.github@gmail.com>
2016-02-21 23:05:33 +0100

    Styling Components

--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -35,7 +35,7 @@ export default class Note extends React.Component {
 		const onDelete = this.props.onDelete;
 		return(
 			<div onClick={this.edit}>
-			<span>{this.props.task}</span>
+			<span className="task">{this.props.task}</span>
 			{onDelete ? this.renderDelete() : null}
 			
 			</div>
@@ -44,7 +44,7 @@ export default class Note extends React.Component {
 	};
 	
 	renderDelete = () => {
-		return <button onClick={this.props.onDelete}>x</button>
+		return <button className="delete-note" onClick={this.props.onDelete}>x</button>
 	};
 	
 	edit = () => {
