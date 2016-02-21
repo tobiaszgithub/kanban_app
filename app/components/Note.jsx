54c5e9cc70e637c36c8d78e002f5bfd364001ec5
tobiaszgithub <tobiasz.github@gmail.com>
2016-02-21 22:23:58 +0100

    Implementing a Basic Note Application

--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -1,3 +1,74 @@
 import React from 'react';
 
-export default () => <div>Learn Webpack!!</div>;
+
+export default class Note extends React.Component {
+	constructor(props){
+		super(props);
+		
+		this.state = {
+			editing: false
+		};
+	}
+	
+	render(){
+		if(this.state.editing){
+			return this.renderEdit();
+		}
+
+		return this.renderNote();
+	}
+	
+	renderEdit = () => {
+		return <input type="text"
+        ref={
+			(e) => e ? e.selectionStart = this.props.task.length : null
+        }
+		autoFocus={true}
+		defaultValue={this.props.task}
+		onBlur={this.finishEdit}
+		onKeyPress={this.checkEnter}
+		/>;
+	}
+	
+	renderNote = () => {
+
+		const onDelete = this.props.onDelete;
+		return(
+			<div onClick={this.edit}>
+			<span>{this.props.task}</span>
+			{onDelete ? this.renderDelete() : null}
+			
+			</div>
+		);
+		
+	};
+	
+	renderDelete = () => {
+		return <button onClick={this.props.onDelete}>x</button>
+	};
+	
+	edit = () => {
+		this.setState({editing:true});
+	}
+	
+	  checkEnter = (e) => {
+		// The user hit *enter*, let's finish up.
+		if(e.key === 'Enter') {
+		  this.finishEdit(e);
+		}
+	  };
+	  
+	finishEdit = (e) =>{
+		debugger;
+		const value = e.target.value;
+		if(this.props.onEdit){
+			this.props.onEdit(value);
+
+			// Exit edit mode.
+		    this.setState({
+			  editing: false
+		    });
+		}
+	}
+	
+}
---
+++ app/components/Notes.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Note from './Note.jsx';
+
+export default ({notes, onEdit, onDelete}) => {
+	return (
+		<ul>{notes.map(note=>
+		<li key={note.id}>
+			<Note task={note.task}
+			onEdit={onEdit.bind(null, note.id)}
+			onDelete={onDelete.bind(null, note.id)}/>
+		</li>
+		)}</ul>
+	);
+}
